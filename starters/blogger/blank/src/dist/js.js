bcb1eb3fad2fb884803dd7ba173090989141a202
ALOTOOL <alotool360@gmail.com>
2020-12-01 19:15:35 +0700

    Initial commit

--- starters/blogger/blank/src/dist/js.js
+++ starters/blogger/blank/src/dist/js.js
@@ -10,4 +10,4 @@
  * License Type    : MIT
  * License URL     : https://opensource.org/licenses/MIT
  */
-!function r(e,n,t){function o(u,f){if(!n[u]){if(!e[u]){var c="function"==typeof require&&require;if(!f&&c)return c(u,!0);if(i)return i(u,!0);var a=new Error("Cannot find module '"+u+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[u]={exports:{}};e[u][0].call(p.exports,(function(r){return o(e[u][1][r]||r)}),p,p.exports,r,e,n,t)}return n[u].exports}for(var i="function"==typeof require&&require,u=0;u<t.length;u++)o(t[u]);return o}({1:[function(r,e,n){r("./js-in-template")},{"./js-in-template":2}],2:[function(r,e,n){},{}]},{},[1]);
+!function r(e,t,n){function i(u,f){if(!t[u]){if(!e[u]){var c="function"==typeof require&&require;if(!f&&c)return c(u,!0);if(o)return o(u,!0);var a=new Error("Cannot find module '"+u+"'");throw a.code="MODULE_NOT_FOUND",a}var p=t[u]={exports:{}};e[u][0].call(p.exports,(function(r){return i(e[u][1][r]||r)}),p,p.exports,r,e,t,n)}return t[u].exports}for(var o="function"==typeof require&&require,u=0;u<n.length;u++)i(n[u]);return i}({1:[function(r,e,t){"use strict";r("./js-in-template")},{"./js-in-template":2}],2:[function(r,e,t){},{}]},{},[1]);
